294541275664695a1aac89f9b6c718c4a195fe33
marce1l <mikesmarcell@gmail.com>
2022-08-13 00:10:52 +0200

    New Add Item Form
    Created new Form for Adding Items

---
+++ Prius-Service/Prius-Service/AddItemPopup.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+namespace Prius_Service
+{
+    partial class AddItemPopup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.VonalkodBeolvas_Button = new System.Windows.Forms.Button();
+            this.nev_TextBox = new System.Windows.Forms.TextBox();
+            this.cikkszam_TextBox = new System.Windows.Forms.TextBox();
+            this.vonalkod_textBox = new System.Windows.Forms.TextBox();
+            this.marka_TextBox = new System.Windows.Forms.TextBox();
+            this.darabszam_TextBox = new System.Windows.Forms.TextBox();
+            this.nev_label = new System.Windows.Forms.Label();
+            this.cikkszam_label = new System.Windows.Forms.Label();
+            this.vonalkod_label = new System.Windows.Forms.Label();
+            this.marka_label = new System.Windows.Forms.Label();
+            this.darabszam_label = new System.Windows.Forms.Label();
+            this.ár_label = new System.Windows.Forms.Label();
+            this.ar_textBox = new System.Windows.Forms.TextBox();
+            this.Mentes_Button = new System.Windows.Forms.Button();
+            this.Elvetes_Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // VonalkodBeolvas_Button
+            // 
+            this.VonalkodBeolvas_Button.Location = new System.Drawing.Point(479, 418);
+            this.VonalkodBeolvas_Button.Name = "VonalkodBeolvas_Button";
+            this.VonalkodBeolvas_Button.Size = new System.Drawing.Size(75, 23);
+            this.VonalkodBeolvas_Button.TabIndex = 0;
+            this.VonalkodBeolvas_Button.Text = "Beolvas";
+            this.VonalkodBeolvas_Button.UseVisualStyleBackColor = true;
+            // 
+            // nev_TextBox
+            // 
+            this.nev_TextBox.Location = new System.Drawing.Point(54, 211);
+            this.nev_TextBox.Name = "nev_TextBox";
+            this.nev_TextBox.Size = new System.Drawing.Size(100, 23);
+            this.nev_TextBox.TabIndex = 1;
+            // 
+            // cikkszam_TextBox
+            // 
+            this.cikkszam_TextBox.Location = new System.Drawing.Point(235, 211);
+            this.cikkszam_TextBox.Name = "cikkszam_TextBox";
+            this.cikkszam_TextBox.Size = new System.Drawing.Size(100, 23);
+            this.cikkszam_TextBox.TabIndex = 2;
+            // 
+            // vonalkod_textBox
+            // 
+            this.vonalkod_textBox.Location = new System.Drawing.Point(313, 418);
+            this.vonalkod_textBox.Name = "vonalkod_textBox";
+            this.vonalkod_textBox.Size = new System.Drawing.Size(100, 23);
+            this.vonalkod_textBox.TabIndex = 3;
+            // 
+            // marka_TextBox
+            // 
+            this.marka_TextBox.Location = new System.Drawing.Point(593, 211);
+            this.marka_TextBox.Name = "marka_TextBox";
+            this.marka_TextBox.Size = new System.Drawing.Size(100, 23);
+            this.marka_TextBox.TabIndex = 4;
+            // 
+            // darabszam_TextBox
+            // 
+            this.darabszam_TextBox.Location = new System.Drawing.Point(778, 211);
+            this.darabszam_TextBox.Name = "darabszam_TextBox";
+            this.darabszam_TextBox.Size = new System.Drawing.Size(100, 23);
+            this.darabszam_TextBox.TabIndex = 5;
+            // 
+            // nev_label
+            // 
+            this.nev_label.AutoSize = true;
+            this.nev_label.Location = new System.Drawing.Point(54, 190);
+            this.nev_label.Name = "nev_label";
+            this.nev_label.Size = new System.Drawing.Size(28, 15);
+            this.nev_label.TabIndex = 6;
+            this.nev_label.Text = "Név";
+            // 
+            // cikkszam_label
+            // 
+            this.cikkszam_label.AutoSize = true;
+            this.cikkszam_label.Location = new System.Drawing.Point(235, 189);
+            this.cikkszam_label.Name = "cikkszam_label";
+            this.cikkszam_label.Size = new System.Drawing.Size(57, 15);
+            this.cikkszam_label.TabIndex = 7;
+            this.cikkszam_label.Text = "Cikkszám";
+            // 
+            // vonalkod_label
+            // 
+            this.vonalkod_label.AutoSize = true;
+            this.vonalkod_label.Location = new System.Drawing.Point(313, 394);
+            this.vonalkod_label.Name = "vonalkod_label";
+            this.vonalkod_label.Size = new System.Drawing.Size(56, 15);
+            this.vonalkod_label.TabIndex = 8;
+            this.vonalkod_label.Text = "Vonalkód";
+            // 
+            // marka_label
+            // 
+            this.marka_label.AutoSize = true;
+            this.marka_label.Location = new System.Drawing.Point(593, 190);
+            this.marka_label.Name = "marka_label";
+            this.marka_label.Size = new System.Drawing.Size(40, 15);
+            this.marka_label.TabIndex = 9;
+            this.marka_label.Text = "Márka";
+            // 
+            // darabszam_label
+            // 
+            this.darabszam_label.AutoSize = true;
+            this.darabszam_label.Location = new System.Drawing.Point(778, 190);
+            this.darabszam_label.Name = "darabszam_label";
+            this.darabszam_label.Size = new System.Drawing.Size(65, 15);
+            this.darabszam_label.TabIndex = 10;
+            this.darabszam_label.Text = "Darabszám";
+            // 
+            // ár_label
+            // 
+            this.ár_label.AutoSize = true;
+            this.ár_label.Location = new System.Drawing.Point(411, 190);
+            this.ár_label.Name = "ár_label";
+            this.ár_label.Size = new System.Drawing.Size(19, 15);
+            this.ár_label.TabIndex = 11;
+            this.ár_label.Text = "Ár";
+            // 
+            // ar_textBox
+            // 
+            this.ar_textBox.Location = new System.Drawing.Point(411, 211);
+            this.ar_textBox.Name = "ar_textBox";
+            this.ar_textBox.Size = new System.Drawing.Size(100, 23);
+            this.ar_textBox.TabIndex = 12;
+            // 
+            // Mentes_Button
+            // 
+            this.Mentes_Button.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.Mentes_Button.Location = new System.Drawing.Point(79, 53);
+            this.Mentes_Button.Name = "Mentes_Button";
+            this.Mentes_Button.Size = new System.Drawing.Size(75, 23);
+            this.Mentes_Button.TabIndex = 13;
+            this.Mentes_Button.Text = "Mentés";
+            this.Mentes_Button.UseVisualStyleBackColor = true;
+            this.Mentes_Button.Click += new System.EventHandler(this.Mentes_Button_Click);
+            // 
+            // Elvetes_Button
+            // 
+            this.Elvetes_Button.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Elvetes_Button.Location = new System.Drawing.Point(217, 53);
+            this.Elvetes_Button.Name = "Elvetes_Button";
+            this.Elvetes_Button.Size = new System.Drawing.Size(75, 23);
+            this.Elvetes_Button.TabIndex = 14;
+            this.Elvetes_Button.Text = "Elvetés";
+            this.Elvetes_Button.UseVisualStyleBackColor = true;
+            // 
+            // AddItemPopup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(930, 568);
+            this.Controls.Add(this.Elvetes_Button);
+            this.Controls.Add(this.Mentes_Button);
+            this.Controls.Add(this.ar_textBox);
+            this.Controls.Add(this.ár_label);
+            this.Controls.Add(this.darabszam_label);
+            this.Controls.Add(this.marka_label);
+            this.Controls.Add(this.vonalkod_label);
+            this.Controls.Add(this.cikkszam_label);
+            this.Controls.Add(this.nev_label);
+            this.Controls.Add(this.darabszam_TextBox);
+            this.Controls.Add(this.marka_TextBox);
+            this.Controls.Add(this.vonalkod_textBox);
+            this.Controls.Add(this.cikkszam_TextBox);
+            this.Controls.Add(this.nev_TextBox);
+            this.Controls.Add(this.VonalkodBeolvas_Button);
+            this.Name = "AddItemPopup";
+            this.Text = "AddItem";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button VonalkodBeolvas_Button;
+        private System.Windows.Forms.TextBox nev_TextBox;
+        private System.Windows.Forms.TextBox cikkszam_TextBox;
+        private System.Windows.Forms.TextBox vonalkod_textBox;
+        private System.Windows.Forms.TextBox marka_TextBox;
+        private System.Windows.Forms.TextBox darabszam_TextBox;
+        private System.Windows.Forms.Label nev_label;
+        private System.Windows.Forms.Label cikkszam_label;
+        private System.Windows.Forms.Label vonalkod_label;
+        private System.Windows.Forms.Label marka_label;
+        private System.Windows.Forms.Label darabszam_label;
+        private System.Windows.Forms.Label ár_label;
+        private System.Windows.Forms.TextBox ar_textBox;
+        private System.Windows.Forms.Button Mentes_Button;
+        private System.Windows.Forms.Button Elvetes_Button;
+    }
+}
---
+++ Prius-Service/Prius-Service/AddItemPopup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Prius_Service
+{
+    public partial class AddItemPopup : Form
+    {
+        public string Nev { get; private set; }
+        public string Cikkszam { get; private set; }
+        public string Vonalkod { get; private set; }
+        public string Marka { get; private set; }
+        public int Darabszam { get; private set; }
+        public int Ar { get; private set; }
+
+
+        public AddItemPopup()
+        {
+            InitializeComponent();
+        }
+
+        private void Mentes_Button_Click(object sender, EventArgs e)
+        {
+            Menu m = new Menu();
+            Nev = nev_TextBox.Text;
+            Cikkszam = cikkszam_TextBox.Text;
+            Vonalkod = vonalkod_textBox.Text;
+            Marka = marka_TextBox.Text;
+            
+            //lekezelni
+            Ar = Convert.ToInt32(ar_textBox.Text);
+            Darabszam = Convert.ToInt32(darabszam_TextBox.Text);
+        }
+
+    }
+}
