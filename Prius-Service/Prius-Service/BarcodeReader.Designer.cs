1a45d8d749dda8ab6fd7b199be1c4e8b71f2c7c6
marce1l <mikesmarcell@gmail.com>
2022-08-13 18:00:23 +0200

    Added BarcodeReader Form

---
+++ Prius-Service/Prius-Service/BarcodeReader.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace Prius_Service
+{
+    partial class BarcodeReader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BarcodeReader));
+            this.barcode_textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // barcode_textBox
+            // 
+            this.barcode_textBox.BackColor = System.Drawing.SystemColors.Window;
+            this.barcode_textBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.barcode_textBox.ForeColor = System.Drawing.SystemColors.Control;
+            this.barcode_textBox.Location = new System.Drawing.Point(148, 153);
+            this.barcode_textBox.Multiline = true;
+            this.barcode_textBox.Name = "barcode_textBox";
+            this.barcode_textBox.Size = new System.Drawing.Size(282, 111);
+            this.barcode_textBox.TabIndex = 0;
+            this.barcode_textBox.TextChanged += new System.EventHandler(this.barcode_textBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(113, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(360, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Olvassa be a termék vonalkódját";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(88, 83);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(417, 257);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // BarcodeReader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 367);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.barcode_textBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "BarcodeReader";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BarcodeReader";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox barcode_textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ Prius-Service/Prius-Service/BarcodeReader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Prius_Service
+{
+    public partial class BarcodeReader : Form
+    {
+        public string barcode { get; private set; }
+        public BarcodeReader()
+        {
+            InitializeComponent();
+        }
+
+        private void barcode_textBox_TextChanged(object sender, EventArgs e)
+        {
+            barcode = barcode_textBox.Text;
+            this.Close();
+        }
+    }
+}
