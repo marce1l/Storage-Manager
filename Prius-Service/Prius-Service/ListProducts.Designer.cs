44585315aa10c15ab365f0a2706a79474edb4652
marce1l <mikesmarcell@gmail.com>
2022-08-15 00:23:32 +0200

    Added List product form

---
+++ Prius-Service/Prius-Service/ListProducts.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace Prius_Service
+{
+    partial class ListProducts
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.dataGridView.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(36, 31);
+            this.dataGridView.MultiSelect = false;
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowTemplate.Height = 25;
+            this.dataGridView.Size = new System.Drawing.Size(911, 497);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // ListProducts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1216, 571);
+            this.Controls.Add(this.dataGridView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "ListProducts";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kilistázott Termékek";
+            this.Load += new System.EventHandler(this.ListProducts_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.DataGridView dataGridView;
+    }
+}
---
+++ Prius-Service/Prius-Service/ListProducts.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Prius_Service
+{
+    public partial class ListProducts : Form
+    {
+        private List<Termek> termekek;
+        public ListProducts(List<Termek> termekek)
+        {
+            this.termekek = termekek;
+            InitializeComponent();
+        }
+
+        private void ListProducts_Load(object sender, EventArgs e)
+        {
+            if (termekek.Count == 0)
+            {
+                MessageBox.Show("szar");
+            }
+            /*
+            Termek t = new Termek("Malkcsh kjgbaxcjg jkxgyc", "l8293v75", "dfhbvcb", "6547546241", 2, 0, 1500000000);
+            Termek z = new Termek("bfxcbc", "j3734k2", "xcxcv", "45769579345", 6, 0, 23512);
+            proba.Add(t);
+            proba.Add(z);
+            */
+            Kilistazas();
+            
+
+        }
+
+        private void Kilistazas()
+        {
+            BindingList<Termek> bindingList = new BindingList<Termek>(termekek);
+            var source = new BindingSource(bindingList, null);
+            dataGridView.DataSource = source;
+
+            dataGridView.Columns[0].HeaderText = "Név";
+            dataGridView.Columns[1].HeaderText = "Cikkszám";
+            dataGridView.Columns[2].HeaderText = "Márka";
+            dataGridView.Columns[3].HeaderText = "Vonalkód";
+            dataGridView.Columns[4].HeaderText = "Darabszám";
+            dataGridView.Columns[5].HeaderText = "Minimum Darabszám";
+            dataGridView.Columns[6].HeaderText = "Ár";
+
+            dataGridView.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView.Columns[6].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+            dataGridView.ClearSelection();
+        }
+    }
+}
